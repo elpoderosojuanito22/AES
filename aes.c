92d999af71754bd579daccadf36c8073e9dbc9b7
elpoderosojuanito22 <godsoord@statloan.info>
2019-05-13 20:19:11 -0500

    funcion de expansion de llave terminada

--- aes.c
+++ aes.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv){
 
 	//printf("in main\n");
 	BYTE llave[16];
-	WORD entrada[44];
+	WORD subLlaves[44];
 	/*
 	for (char i = 0; i < 16; i++)
 	{
@@ -33,7 +33,12 @@ int main(int argc, char **argv){
 	llave[14] = 0x4f;
 	llave[15] = 0x3c;
 
-	KeyExpansion(llave, entrada, 4);
+	KeyExpansion(llave, subLlaves, 4);
+	for (int j = 0; j < sizeof(subLlaves)/sizeof(subLlaves[0]); j++)
+	{
+		printf("w[%i] = %x \n",j, subLlaves[j]);
+	}
+	
 	//printf("CHAR_BIT    :   %d\n", CHAR_BIT);
 }
 
@@ -50,22 +55,33 @@ void KeyExpansion(BYTE key[4 * Nk], WORD w[Nb * (Nr + 1)], int Nk1){
 		key[0] = w[i] & 0xFF;
 		*/
 		w[i] = (key[4 * i] << 24) | (key[4 * i + 1] << 16) | (key[4 * i + 2] << 8 | (key[4 * i + 3] << 0));
-		printf("%x \n", w[i]);
+		//printf("%x \n", w[i]);
 		i = i + 1;
 		//w[i] = WORD(key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]);
 	}
+	//printf("w[0] = %x \n", w[0]);
+	//printf("w[1] = %x \n", w[1]);
+	//printf("w[2] = %x \n", w[2]);
+	//printf("w[3] = %x \n", w[3]);
 	i = Nk;
-	//while (i < Nb * (Nr + 1)){
+	while (i < Nb * (Nr + 1)){
 		temp = w[i - 1];
-		printf("temp = %x \n", temp);
+		//printf("temp = %x \n", temp);
 		if(i % Nk == 0){
 			RotWord(&temp);
-			printf("temp con rotword = %x \n", temp);
+			//printf("temp con rotword = %x \n", temp);
 			SubWord(&temp);
-			printf("con subword = %x \n", temp);
+			//printf("con subword = %x \n", temp);
+			temp = temp ^ Rcon[i/Nk] ;
+			//printf("con xor Rcon = %x \n", temp);
+			//printf("i / Nk = %x \n", i/Nk);
+		}else if ((Nk>6) && (i%Nk ==4)){  
+			SubWord(&temp);
 		}
-		
-	//}
+		w[i] = w[i-Nk] ^ temp;
+		//printf("w[%i] = %x \n",i, w[i]);
+		i = i + 1;
+	}
 	printf("out key expansion\n");
 }
 
